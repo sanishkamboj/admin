8e698e74579b6cd0cc68653102538eef39a30f75
Sanish <sanish@github.com>
2020-07-17 16:07:17 +0530

    new changes

--- resources/views/user/list.blade.php
+++ resources/views/user/list.blade.php
@@ -5,7 +5,7 @@
             <div class="col-md-12">
                 <nav aria-label="breadcrumb" class="float-right mt-1">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{route('user.home')}}">Home</a></li>
                         <li class="breadcrumb-item active" aria-current="page">Users</li>
                     </ol>
                 </nav>
